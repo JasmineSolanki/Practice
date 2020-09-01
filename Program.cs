4a57b8418bef4ffd3bf9b1736836f1862c8f46b6
Jasmine Solanki <jasminesolanki2018@gmail.com>
2020-09-01 08:17:58 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Write a program that requests the reading of a series of individual characters and count 
+//howmany times the letter 'a' is entered. The user ends by entering the "$" symbol
+
+using System;
+namespace CountChar_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0;
+            while (true)
+            {
+                Console.WriteLine("Enter the alphabet");
+                char C= Convert.ToChar(Console.ReadLine());
+                if (C== 'a')
+               {
+                count++;
+               }
+               else if (C=='$')
+               break;
+            }
+            Console.WriteLine(" the total number of charcter 'a' is " +count);
+        }
+    }
+}
